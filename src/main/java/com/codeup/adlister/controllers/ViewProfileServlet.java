42e6627e37360ac6c551315b914c68cac797ab7e
Robert De la Rosa <rdelarosa39@gmail.com>
2020-11-18 09:36:40 -0600

    Feat: logged in user can edit profilie and view others profiles

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -15,16 +15,29 @@ import java.io.IOException;
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User currentUser  = (User) request.getSession().getAttribute("user");
+        User profileOwner = null;
+        // redirect unregistered users to login
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
             return;
         }
-        User  currentUser  = (User) request.getSession().getAttribute("user");
-        request.setAttribute("ads", DaoFactory.getAdsDao().userAds( currentUser.getId())); //sets attribute to call the ads
-
-
+        System.out.println(request.getParameter("author"));
+        // if ad userId parameter set profile owner to author else profile owner is the logged in user
+        if (request.getParameter("author") != null ){
+           profileOwner = DaoFactory.getUsersDao().findByUserId(Long.parseLong(request.getParameter("author")));
+           if(profileOwner == null ){
+               response.sendRedirect("/profile");
+               return;
+           }
+        }
+        if(profileOwner == null){
+            profileOwner = currentUser;
+        }
+        
+        request.setAttribute("profileOwner",profileOwner);
+        request.setAttribute("ads", DaoFactory.getAdsDao().userAds( profileOwner.getId()));
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-
     }
 
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -3,25 +3,35 @@
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Your Profile" />
+        <jsp:param name="title" value="${profileOwner.username}>" />
     </jsp:include>
 </head>
 <body>
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Welcome, ${sessionScope.user.username}!</h1>
+        <div class="col-md-4">
+                <img src="${pageContext.servletContext.contextPath}/src/main/resources/img/default_avatar.jpg" alt="Avatar" class="rounded-circle">
+                <br>
+                <br>
+                <p> Joined on: </p>
+                <c:if test = "${profileOwner.id == sessionScope.user.id}">
+                    <a href="/profile/update">Edit Profile</a>
+                </c:if>
+
+        </div>
+        <h1><c:out value="${profileOwner.username}"/>!</h1>
     </div>
 
     <div class="container">
         <h1>Ads</h1>
-            <c:forEach var="ad" items="${ads}">
-                <div class="col-md-6">
-                    <h3>${ad.title}</h3>
-                    <p>${ad.description}</p>
-            </c:forEach>
-        </div>
+        <c:forEach var="ad" items="${ads}">
+            <div class="col-md-6">
+                <h3><c:out value ="${ad.title}"/></h3>
+                <p><c:out value ="${ad.description}"/></p>
+            </div>
+        </c:forEach>
     </div>
-
+<jsp:include page="partials/footer.jsp"/>
 </body>
 </html>
